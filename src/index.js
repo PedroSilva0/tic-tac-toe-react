64326bdd1283f96860bfbf2bf31a7ade7134982b
Pedro Silva <pedrocas-silva@hotmail.com>
2022-01-12 17:10:38 +0000

    Changed board construction to use for loops

--- src/index.js
+++ src/index.js
@@ -19,26 +19,25 @@ class Board extends React.Component {
       />;
 }
 
+  renderRow(i){
+    let squares = []
+    
+    for (let j = 0; j<3 ; j++){
+      squares.push(this.renderSquare(i*3+j))
+    } 
+
+    return <div className="board-row">{squares}</div>
+  }
+
+
+
   render() {
-    return (
-    <div>
-        <div className="board-row">
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-        </div>
-    </div>
-    );
+    let rows = [];
+    for (let i = 0;i<3;i++){
+      rows.push(this.renderRow(i))
+    }
+
+    return <div>{rows}</div>
   }
 }
 
